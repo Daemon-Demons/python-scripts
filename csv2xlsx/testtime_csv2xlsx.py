e95387435ddfbe2aa43ff93c8c3ff0ecd14695c0
siva <siva52.shri238@gmail.com>
2021-11-25 01:38:26 +0530

    passing arguments and help message

--- csv2xlsx/testtime_csv2xlsx.py
+++ csv2xlsx/testtime_csv2xlsx.py
@@ -1,1 +1,16 @@
-print('Hello world!')
+import os
+import sys
+# import pandas as pd
+# import numpy as np
+
+#help message
+help_message = """python3 testtime_csv2xlsx.py -i <file.csv> -o <file.xlsx>
+    -i  :   input csv file
+    -o  :   output csv file/path
+    -h  :   prints this message
+ """
+if (len(sys.argv) == 2):
+    if(sys.argv[1] == "-h"):
+        print('Test time crunching tool\n', help_message)
+    else:
+        print('Please input argument\n', help_message)
